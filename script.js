9bdf4bbc345718c66b23324f99e4bf8aa5af5511
ChaitanyaEmani <131893680+ChaitanyaEmani@users.noreply.github.com>
2023-12-13 15:53:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let qrtext = document.getElementById("qrtext");
+let imgbox = document.getElementById("imgbox");
+let qrimage = document.getElementById("qrimage");
+
+function qrgenerator(){
+    if(qrtext.value.length > 0){
+    qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrtext.Value;
+    imgbox.classList.add("show-img");
+    }
+    else{
+        qrtext.classList.add('error');
+        setTimeout(()=>{
+            qrtext.classList.remove('error');
+        },1000);
+    }
+}
